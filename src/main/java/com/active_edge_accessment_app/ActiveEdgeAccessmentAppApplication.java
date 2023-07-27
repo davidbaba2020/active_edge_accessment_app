b58942d1c40d1da3c6949f57932ef8741a9584c9
davidbaba <davidbabasalifu2020@gmail.com>
2023-07-27 08:31:47 +0100

    Initial commit

---
+++ src/main/java/com/active_edge_accessment_app/ActiveEdgeAccessmentAppApplication.java
@@ -0,0 +1,13 @@
+package com.active_edge_accessment_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActiveEdgeAccessmentAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActiveEdgeAccessmentAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/active_edge_accessment_app/ActiveEdgeAccessmentAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.active_edge_accessment_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActiveEdgeAccessmentAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
